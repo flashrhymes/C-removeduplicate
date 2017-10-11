44ea71e75989c4626a896b035d52cee1ad7c9d18
flashrhymes <grandbossgrim@gmail.com>
2017-10-11 13:51:41 +0300

    remove duplicate

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateRemove1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> adlar = new List<string> {"tural","Nicat","Yasin","Hafiz","Muzeyyen","Yasin", "Tural", "nicat"};
+            adlar.Sort();
+          
+            var kopyalar = adlar.Distinct(StringComparer.OrdinalIgnoreCase).ToArray();
+            
+
+            foreach (string item in kopyalar)
+            {
+                Console.WriteLine(item.First().ToString().ToUpper() + item.Substring(1));
+            }
+
+            
+
+            
+
+
+        }
+        
+        
+    }
+}
